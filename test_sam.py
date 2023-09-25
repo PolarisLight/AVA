16038cbd803cc3bf0e08c61499b1164ae603f599
PolarisLight <873237327@qq.com>
2023-09-25 16:55:38 +0800

    debug

--- test_sam.py
+++ test_sam.py
@@ -22,27 +22,28 @@ def show_anns(anns):
     ax.imshow(img)
 
 
-img_name = "53.jpg"
+img_name = "77.jpg"
 img = cv2.imread("dataset/images/" + img_name)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 print(img.shape)
 
 sam = sam_model_registry["default"](checkpoint="sam_vit_h_4b8939.pth")
+device = "mps"
+sam.to(device)
 mask_generator = SamAutomaticMaskGenerator(sam, min_mask_region_area=50)
 tic = time.time()
 masks = mask_generator.generate(img)
 
 print(f'everything prompt time: {time.time() - tic}')
 
+
+plt.figure(figsize=(20, 20))
+plt.imshow(img)
+show_anns(masks)
+plt.axis('off')
+plt.savefig("output/"+img_name, bbox_inches='tight')
+plt.show()
 for mask in masks:
     mask["segmentation"] = mask["segmentation"].tolist()
 with open(img_name + ".json", "w") as f:
     json.dump(masks, f)
-# plt.figure(figsize=(20, 20))
-# plt.imshow(img)
-# show_anns(masks)
-# plt.axis('off')
-# plt.savefig("output/"+img_name, bbox_inches='tight')
-# plt.show()
-# for mask in masks:
-#     img = cv2.rectangle(img,mask["bbox"])
