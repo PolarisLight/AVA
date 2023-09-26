1e341dccccc099dd9ea17e60a4c367425f4843f9
PolarisLight <873237327@qq.com>
2023-09-26 11:00:22 +0800

    debug

--- sam_encoder.py
+++ sam_encoder.py
@@ -17,14 +17,14 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 sam.to(device)
 for layer in sam.image_encoder.children():
     print(layer)
+for img_name in img_files:
+    img = cv2.imread(img_name)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (1024 , 1024))
+    img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).float().to(device)
+    print(img.shape)
 
-img = cv2.imread("dataset/images/77.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = cv2.resize(img, (1024 , 1024))
-img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).float().to(device)
-print(img.shape)
-
-tic = time.time()
-embeddings = sam.image_encoder(img)
-print(f'generate time: {time.time() - tic}')
-print(embeddings.shape)
+    tic = time.time()
+    embeddings = sam.image_encoder(img)
+    print(f'generate time: {time.time() - tic}')
+    print(embeddings.shape)
