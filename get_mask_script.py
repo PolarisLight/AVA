0b70bc7fb90a8e80ef04ea54a9cdedce06674b72
PolarisLight <873237327@qq.com>
2023-11-09 13:46:10 +0800

    update get mask script to handle undetect situation

--- get_mask_script.py
+++ get_mask_script.py
@@ -74,21 +74,20 @@ def get_img_masks(img_name):
     return
 
 
-# if __name__ == "__main__":
-#     finished_list = glob.glob(save_root + "*.npz")
-#     with tqdm.tqdm(total=len(img_files)) as pbar:
-#         for i, img_file in enumerate(img_files):
-#             img_name = os.path.basename(img_file)
-#             name_without_format = img_name.split(".")[0]
-#             if save_root + name_without_format + ".npz" in finished_list:
-#                 print(f"{img_name} has been processed")
-#                 pbar.update(1)
-#                 continue
-#             pbar.set_description(f"Processing {img_name}")
-#             get_img_masks(img_file)
-#
-#             pbar.update(1)
-#             # if i > 1:
-#             #     break
-#
-get_img_masks("F:/Dataset/AVA/images/427243.jpg")
+if __name__ == "__main__":
+    finished_list = glob.glob(save_root + "*.npz")
+    with tqdm.tqdm(total=len(img_files)) as pbar:
+        for i, img_file in enumerate(img_files):
+            img_name = os.path.basename(img_file)
+            name_without_format = img_name.split(".")[0]
+            if save_root + name_without_format + ".npz" in finished_list:
+                print(f"{img_name} has been processed")
+                pbar.update(1)
+                continue
+            pbar.set_description(f"Processing {img_name}")
+            get_img_masks(img_file)
+
+            pbar.update(1)
+            # if i > 1:
+            #     break
+
